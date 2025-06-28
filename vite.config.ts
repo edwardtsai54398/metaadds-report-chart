bfcb48f4b05d5f986f51898070ccb2fab839fe72
LAPTOP-QB1B92GD\Edward <fashion.donk@gmail.com>
2025-06-28 09:56:47 +0800

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/metaads-report-chart-generator' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/metaads-report-chart/' : '/',
   plugins: [
     react(), 
     tailwindcss(), 
